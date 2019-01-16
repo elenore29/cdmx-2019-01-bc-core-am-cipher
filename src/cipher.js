0270c48f258a6203a07f577233250fe4cc154bf7
elenore29 <elenore.nava@gmail.com>
2019-01-16 17:39:39 -0600

    ya codifica y corre el test

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,43 @@
+// Se declara la variable nuevoCifrado de forma global y se inicializa como una string vacía
+let nuevoCifrado="";
 window.cipher = {
-  // ... 
+//Se crea la función encode
+encode: (offset, str)=>{
+  //La variable offset se convierte en numero entero
+  let offsetNuevo = parseInt(offset);
+  let mayuscula= str.toUpperCase();
+/* Se crea un ciclo for donde se aplica la formula para trasladar
+la posicion de la letra en codigo ascii
+se inicializa i en 0 y va aumentando una posicion conforme aumente
+el largo del String*/
+    for(let i=0; i<mayuscula.length; i++){
+        let letraAscii = mayuscula.charCodeAt(i);
+        let formula = (letraAscii-65+offsetNuevo)%26+65;
+        let resultado = String.fromCharCode(formula);
+        nuevoCifrado+=resultado;
+    }
+    return (nuevoCifrado);
+},
+  decode: (offset, str)=>{
+    let offsetNuevo = parseInt(offset)
+    let mayuscula = str.toUpperCase();
+
+      for(let i=0; i<mayuscula.length; i++){
+          let letraAscii = mayuscula.charCodeAt(i);
+          let formula = (letraAscii+65-offsetNuevo)%26+65;
+          let resultado = String.fromCharCode(formula);
+          nuevoCifrado+=resultado;
+      }
+      console.log(nuevoCifrado)
+      return (nuevoCifrado);
+    }
 };
+
+/*
+window.cipher = {
+  // ...
+  encode: (offset, str)=> {},
+  decode: (offset, str)=> {}
+
+};
+*/
---
+++ src/index.js
@@ -0,0 +1,48 @@
+const pantalla1 = document.getElementById('pantalla1');
+const pantalla2= document.getElementById('pantalla2');
+const pantalla3 = document.getElementById('pantalla3');
+const codificar = document.getElementById('azul');
+const morado = document.getElementById('morado');
+const decodificar = document.getElementById('verde');
+
+//Se declara una variable por cada objeto del HTML (inputs y parrafo)  que se llama mediante su ID
+const str1 = document.getElementById("mensaje");
+/*Se llama al objeto HTML donde el usuario va a ingresar o elegir el offset
+mediante su id, definiendose la variable offset */
+let offset1 = document.getElementById("opcion");
+
+
+
+codificar.addEventListener('click',()=> {
+  let offset=offset1.value;
+  let str=str1.value;
+  pantalla1.classList.add('hide');
+  pantalla3.classList.add('hide');
+  pantalla2.classList.remove('hide');
+  let resultadoCifrado = window.cipher.encode(offset, str);
+  document.getElementById("msjcifrado").innerHTML=resultadoCifrado;
+});
+
+ morado.addEventListener('click',()=> {
+  pantalla3.classList.add('hide');
+  pantalla2.classList.add('hide');
+  pantalla1.classList.remove('hide');
+});
+
+ decodificar.addEventListener('click',()=> {
+  let offset=offset1.value;
+  let str=str1.value;
+  pantalla1.classList.add('hide');
+  pantalla2.classList.add('hide');
+  pantalla3.classList.remove('hide');
+  let resultadoCifrado = window.cipher.decode(offset, str);
+  document.getElementById("msjdescifrado").innerHTML=resultadoCifrado;
+});
+
+
+// addEventListener('click', ()=>{
+//   window.cipher.encode(offset, str)
+// })
+
+//document.getElementById("msjcifrado").innerHTML=nuevoCifrado;
+//document.getElementById("msjdescifrado").innerHTML=nuevoCifrado;
