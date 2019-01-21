0bbc6547e25187bca8382d569c9a30b72e26c20f
elenore29 <elenore.nava@gmail.com>
2019-01-21 16:46:53 -0600

    Ya cifra mayusculas y minusculas, respeta los simbolos, descifra mayusculas y pasa los test correspondientes pero aun hay un error en la formula para descifrar minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -5,43 +5,57 @@ encode: (offset, str)=>{
   let nuevoCifrado="";
   //La variable offset se convierte en numero entero
   let offsetNuevo = parseInt(offset);
-  let mayuscula= str.toUpperCase();
+
 /* Se crea un ciclo for donde se aplica la formula para trasladar
 la posicion de la letra en codigo ascii
 se inicializa i en 0 y va aumentando una posicion conforme aumente
 el largo del String*/
-    for(let i=0; i<mayuscula.length; i++){
-        let letraAscii = mayuscula.charCodeAt(i);
-          if (letraAscii == 32){
-            let resultado = String.fromCharCode(letraAscii);
-            nuevoCifrado+=resultado;
-          }
-          else {
-            let formula = (letraAscii-65+offsetNuevo)%26+65;
-            let resultado = String.fromCharCode(formula);
-            nuevoCifrado+=resultado;
-          }
+for(let i=0; i<str.length; i++){
+    let letraAscii = str.charCodeAt(i);
 
+    if (letraAscii >=65 && letraAscii<=90){
+      let formula = (letraAscii-65+offsetNuevo)%26+65;
+      let resultado = String.fromCharCode(formula);
+      nuevoCifrado+=resultado;
     }
+
+    else if (letraAscii >=97 && letraAscii<=122) {
+      let formula = (letraAscii-97+offsetNuevo)%26+97;
+      let resultado = String.fromCharCode(formula);
+      nuevoCifrado+=resultado;
+    }
+
+    else {
+      let resultado = String.fromCharCode(letraAscii);
+      nuevoCifrado+=resultado;
+  }
+
+}
     return (nuevoCifrado);
 },
   decode: (offset, str)=>{
     let nuevoCifrado="";
     let offsetNuevo = parseInt(offset)
-    let mayuscula = str.toUpperCase();
 
-      for(let i=0; i<mayuscula.length; i++){
-          let letraAscii = mayuscula.charCodeAt(i);
-            if (letraAscii == 32){
-              let resultado = String.fromCharCode(letraAscii);
-              nuevoCifrado+=resultado;
-          }
-          else {
+
+      for(let i=0; i<str.length; i++){
+          let letraAscii = str.charCodeAt(i);
+
+          if (letraAscii >=65 && letraAscii<=90){
             let formula = (letraAscii+65-offsetNuevo)%26+65;
             let resultado = String.fromCharCode(formula);
             nuevoCifrado+=resultado;
           }
 
+          else if (letraAscii >=97 && letraAscii<=122) {
+            let formula = (letraAscii+97+offsetNuevo)%26+97;
+            let resultado = String.fromCharCode(formula);
+            nuevoCifrado+=resultado;
+          }
+          else {
+            let resultado = String.fromCharCode(letraAscii);
+            nuevoCifrado+=resultado;
+          }
       }
       return (nuevoCifrado);
     }
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -15,12 +15,23 @@ describe('cipher', () => {
    });
       // Hacker edition
     //
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
+  // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
     //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   assert.equal(cipher.encode(33, ' !@'),' !@');
-    // });
+     it('debería retornar " !@" para " !@"', () => {
+      assert.equal(cipher.encode(33, ' !@'),' !@');
+     });
+
+  // Hacker edition
+     // Si decides agregar soporte para minúsculas descomenta el test a
+     // continuación.
+     //
+     it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+       assert.equal(
+         cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'),
+         'hijklmnopqrstuvwxyzabcdefg'
+      );
+     });
 
     //
     // Hacker edition
@@ -48,12 +59,12 @@ describe('cipher', () => {
     // Si decides agregar soporte para minúsculas descomenta el test a
     // continuación.
     //
-    // it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
-    //   assert.equal(
-    //     cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
-    //     'abcdefghijklmnopqrstuvwxyz'
-    //   );
-    // });
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+      assert.equal(
+        cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
+        'abcdefghijklmnopqrstuvwxyz'
+     );
+    });
 
     //
     // Hacker edition
@@ -61,9 +72,9 @@ describe('cipher', () => {
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
     //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   assert.equal(cipher.decode(33, ' !@'),' !@');
-    // });
+    it('debería retornar " !@" para " !@"', () => {
+      assert.equal(cipher.decode(33, ' !@'),' !@');
+    });
   });
 
 });
