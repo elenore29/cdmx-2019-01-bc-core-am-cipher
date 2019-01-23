b2c5c40b903a5e01f7ce8d82bb19a1ad11f439d8
elenore29 <elenore.nava@gmail.com>
2019-01-23 15:24:00 -0600

    Implemente la pantalla de inicio con el boton comenzar

--- src/cipher.js
+++ src/cipher.js
@@ -1,71 +1,74 @@
-// Se declara la variable nuevoCifrado de forma global y se inicializa como una string vacía
 window.cipher = {
-//Se crea la función encode
-encode: (offset, str)=>{
-  let nuevoCifrado="";
-  //La variable offset se convierte en numero entero
-  let offsetNuevo = parseInt(offset);
+  //Se crea la función encode
+  encode: (offset, str) => {
+    // Se inicializa la variable nuevoCifrado que va a contener el resultado de la función
+    let nuevoCifrado = "";
+    //La variable offset se convierte en número entero
+    let offsetNuevo = parseInt(offset);
 
-/* Se crea un ciclo for donde se aplica la formula para trasladar
-la posicion de la letra en codigo ascii
-se inicializa i en 0 y va aumentando una posicion conforme aumente
-el largo del String*/
-for(let i=0; i<str.length; i++){
-    let letraAscii = str.charCodeAt(i);
-
-    if (letraAscii >=65 && letraAscii<=90){
-      let formula = (letraAscii-65+offsetNuevo)%26+65;
-      let resultado = String.fromCharCode(formula);
-      nuevoCifrado+=resultado;
+    /* Se crea un ciclo for donde obtiene el código ascii de cada carácter
+    se inicializa i en 0 y va aumentando una posición conforme aumente
+    el largo del String*/
+    for (let i = 0; i < str.length; i++) {
+      let letraAscii = str.charCodeAt(i);
+      /*Se implementa un if que indica que si el código ascii del carácter ingresado
+      esta entre el rango 65 y 90 (A-Z), se aplique la formula correspondiente para obtener su nueva posición en el código ascii
+      y que devuelva el carácter de esa posición*/
+      if (letraAscii >= 65 && letraAscii <= 90) {
+        let formula = (letraAscii - 65 + offsetNuevo) % 26 + 65;
+        let resultado = String.fromCharCode(formula);
+        nuevoCifrado += resultado;
+      }
+      /*Se implementa un else if para que si el código ascii del carácter ingresado
+      esta entre el rango 97 y 122 (a-z), se aplique la formula correspondiente para obtener su nueva posición en el código ascii
+      y que devuelva el carácter de esa posición*/
+      else if (letraAscii >= 97 && letraAscii <= 122) {
+        let formula = (letraAscii - 97 + offsetNuevo) % 26 + 97;
+        let resultado = String.fromCharCode(formula);
+        nuevoCifrado += resultado;
+      }
+      /*Se implementa un else para que si no se cumplen las primeras dos condiciones,
+      devuelva el mismo carácter*/
+      else {
+        let resultado = String.fromCharCode(letraAscii);
+        nuevoCifrado += resultado;
+      }
     }
-
-    else if (letraAscii >=97 && letraAscii<=122) {
-      let formula = (letraAscii-97+offsetNuevo)%26+97;
-      let resultado = String.fromCharCode(formula);
-      nuevoCifrado+=resultado;
-    }
-
-    else {
-      let resultado = String.fromCharCode(letraAscii);
-      nuevoCifrado+=resultado;
-  }
-
-}
     return (nuevoCifrado);
-},
-  decode: (offset, str)=>{
-    let nuevoCifrado="";
+  },
+  //Se crea la función encode
+  decode: (offset, str) => {
+    // Se inicializa la variable nuevoCifrado que va a contener el resultado de la función
+    let nuevoCifrado = "";
+    //La variable offset se convierte en número entero
     let offsetNuevo = parseInt(offset)
 
-
-      for(let i=0; i<str.length; i++){
-          let letraAscii = str.charCodeAt(i);
-
-          if (letraAscii >=65 && letraAscii<=90){
-            let formula = (letraAscii+65-offsetNuevo)%26+65;
-            let resultado = String.fromCharCode(formula);
-            nuevoCifrado+=resultado;
-          }
-
-          else if (letraAscii >=97 && letraAscii<=122) {
-            let formula = (letraAscii-122-offsetNuevo)%26+122;
-            let resultado = String.fromCharCode(formula);
-            nuevoCifrado+=resultado;
-          }
-          else {
-            let resultado = String.fromCharCode(letraAscii);
-            nuevoCifrado+=resultado;
-          }
+    /* Se crea un ciclo for donde obtiene el código ascii de cada carácter
+    se inicializa i en 0 y va aumentando una posición conforme aumente
+    el largo del String*/
+    for (let i = 0; i < str.length; i++) {
+      let letraAscii = str.charCodeAt(i);
+      /*Se implementa un if que indica que si el código ascii del carácter ingresado
+      esta entre el rango 65 y 90 (A-Z), se aplique la formula correspondiente para obtener su nueva posición en el código ascii
+      y que devuelva el carácter de esa posición*/
+      if (letraAscii >= 65 && letraAscii <= 90) {
+        let formula = (letraAscii + 65 - offsetNuevo) % 26 + 65;
+        let resultado = String.fromCharCode(formula);
+        nuevoCifrado += resultado;
+        /*Se implementa un else if para que si el código ascii del carácter ingresado
+        esta entre el rango 97 y 122 (a-z), se aplique la formula correspondiente para obtener su nueva posición en el código ascii
+        y que devuelva el carácter de esa posición*/
+      } else if (letraAscii >= 97 && letraAscii <= 122) {
+        let formula = (letraAscii - 122 - offsetNuevo) % 26 + 122;
+        let resultado = String.fromCharCode(formula);
+        nuevoCifrado += resultado;
+        /*Se implementa un else para que si no se cumplen las primeras dos condiciones,
+        devuelva el mismo carácter*/
+      } else {
+        let resultado = String.fromCharCode(letraAscii);
+        nuevoCifrado += resultado;
       }
-      return (nuevoCifrado);
     }
+    return (nuevoCifrado);
+  }
 };
-
-/*
-window.cipher = {
-  // ...
-  encode: (offset, str)=> {},
-  decode: (offset, str)=> {}
-
-};
-*/
--- src/index.js
+++ src/index.js
@@ -1,48 +1,52 @@
+/*Se declaran las variables correspondientes para cada sección del html*/
 const pantalla1 = document.getElementById('pantalla1');
-const pantalla2= document.getElementById('pantalla2');
+const pantalla2 = document.getElementById('pantalla2');
 const pantalla3 = document.getElementById('pantalla3');
 
+/*Se declaran las variables correspondientes a cada boton del html*/
 const codificar = document.getElementById('azul');
 const morado = document.getElementById('morado');
 const decodificar = document.getElementById('verde');
 
-//Se declara una variable por cada objeto del HTML (inputs y parrafo)  que se llama mediante su ID
-const str1 = document.getElementById("mensaje");
-/*Se llama al objeto HTML donde el usuario va a ingresar o elegir el offset
-mediante su id, definiendose la variable offset */
-let offset1 = document.getElementById("opcion");
+/*Se declara una variable donde se llama el input del html
+en cual el usuario va a ingresar su texto*/
+const str1 = document.getElementById('mensaje');
 
-codificar.addEventListener('click',()=> {
-  let offset=offset1.value;
-  let str=str1.value;
+/*Se llama al objeto html (donde el usuario va a ingresar o elegir el offset)
+mediante su id*/
+let offset1 = document.getElementById('opcion');
+
+/* Se implementa el evento click para el boton que va a codificar,
+para que lleve deve de la pantalla 1 o 3 a la 2, que es la que muestra el resultado
+y ahí mismo se obtiene el valor del texto y del offset */
+codificar.addEventListener('click', () => {
+  let offset = offset1.value;
+  let str = str1.value;
   pantalla1.classList.add('hide');
   pantalla3.classList.add('hide');
   pantalla2.classList.remove('hide');
   let resultadoCifrado = window.cipher.encode(offset, str);
-  document.getElementById("msjcifrado").innerHTML=resultadoCifrado;
+  document.getElementById("msjcifrado").innerHTML = resultadoCifrado;
 });
 
- morado.addEventListener('click',()=> {
+/* Se implementa el evento click para el boton que va regresar al usuario a la pantalla 1,
+desde la pantalla 2 y 3, y que pueda volver a ingresar un texto y un offset*/
+morado.addEventListener('click', () => {
   location.reload(true);
   pantalla3.classList.add('hide');
   pantalla2.classList.add('hide');
   pantalla1.classList.remove('hide');
 });
 
- decodificar.addEventListener('click',()=> {
-  let offset=offset1.value;
-  let str=str1.value;
+/* Se implementa el evento click para el boton que va a decodificar,
+para que lleve deve de la pantalla 1 o 2 a la 3, que es la que muestra el resultado
+y ahí mismo se obtiene el valor del texto y del offset */
+decodificar.addEventListener('click', () => {
+  let offset = offset1.value;
+  let str = str1.value;
   pantalla1.classList.add('hide');
   pantalla2.classList.add('hide');
   pantalla3.classList.remove('hide');
   let resultadoCifrado = window.cipher.decode(offset, str);
-  document.getElementById("msjdescifrado").innerHTML=resultadoCifrado;
+  document.getElementById("msjdescifrado").innerHTML = resultadoCifrado;
 });
-
-
-// addEventListener('click', ()=>{
-//   window.cipher.encode(offset, str)
-// })
-
-//document.getElementById("msjcifrado").innerHTML=nuevoCifrado;
-//document.getElementById("msjdescifrado").innerHTML=nuevoCifrado;
